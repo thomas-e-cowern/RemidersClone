dd2cdabd2402f68d6debd1dd2622c2ad25d3c8b8
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-10-10 13:40:59 -0400

    update add new list view with comments and toolbar

--- RemindersClone/RemindersClone/Views/AddNewListView.swift
+++ RemindersClone/RemindersClone/Views/AddNewListView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct AddNewListView: View {
     
     // MARK: - Properties
+    @Environment(\.dismiss) private var dismiss
     @State private var name: String = ""
     @State private var selectedColor: Color = .yellow
     
@@ -18,6 +19,7 @@ struct AddNewListView: View {
         VStack {
             VStack {
                 Image(systemName: "line.3.horizontal.circle.fill")
+                    .foregroundColor(selectedColor)
                     .font(.system(size: 100))
                 TextField("List Name", text: $name)
                     .multilineTextAlignment(.center)
@@ -30,10 +32,39 @@ struct AddNewListView: View {
             
             Spacer()
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .toolbar(content: {
+            ToolbarItem(placement: .principal) {
+                Text("New List")
+                    .font(.headline)
+            }
+            
+            // Close view
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button("Close") {
+                    dismiss()
+                }
+            }
+            
+            // Save new list and close view
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button("Done") {
+                    // Save new list
+                }
+                .disabled(!isFormValid)
+            }
+        })
+    }
+    
+    // MARK: - Computed Variables
+    private var isFormValid: Bool {
+        !name.isEmpty
     }
 }
 
 // MARK: Preview
 #Preview {
-    AddNewListView()
+    NavigationStack {
+        AddNewListView()
+    }
 }
