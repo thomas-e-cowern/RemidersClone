3fe86769ffc7c4a7e46180f2f04c38a701549b39
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-10-10 11:43:58 -0400

    restart project

--- RemindersClone/RemindersClone/ContentView.swift
+++ RemindersClone/RemindersClone/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, Everyone!")
+            Text("Hello, world!")
         }
         .padding()
     }
