a72b1decfe3aa4fec3b45bd50da7550fc19e3dc9
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-10-10 11:33:56 -0400

    add core data provider to project

--- RemindersClone/Providers/CoreDataProvider.swift
+++ RemindersClone/Providers/CoreDataProvider.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import CoreData
+
+class CoreDataProvider {
+    
+    // Singleton
+    static let shared = CoreDataProvider()
+    
+    // Persistent container
+    let persistentContainer: NSPersistentContainer
+    
+    // Private init
+    private init() {
+        // Assign persistent container
+        persistentContainer = NSPersistentContainer(name: "RemindersModel")
+        // Load stores
+        persistentContainer.loadPersistentStores { description, error in
+            if let error {
+                fatalError("Error initializing RemindersModel: \(error)")
+            }
+        }
+    }
+    
+}
