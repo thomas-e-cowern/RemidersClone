8f4e2b5a7ca5930c253c79907144506397b1988b
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-10-10 11:17:16 -0400

    remidners clone initial commit

---
+++ RemindersClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RemindersClone
+//
+//  Created by Thomas Cowern on 10/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RemindersClone/RemindersCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersCloneApp.swift
+//  RemindersClone
+//
+//  Created by Thomas Cowern on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct RemindersCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
