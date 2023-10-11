838e3507cadf37fbde4b11ca7e9e856747c7d643
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-10-11 09:57:33 -0400

    update reminder service with save method

--- RemindersClone/RemindersClone/Services/RemindersService.swift
+++ RemindersClone/RemindersClone/Services/RemindersService.swift
@@ -9,4 +9,25 @@ import Foundation
 import CoreData
 import UIKit
 
-
+class RemindersService {
+    
+    // Set up viewContext
+    static var viewContext: NSManagedObjectContext {
+        CoreDataProvider.shared.persistentContainer.viewContext
+    }
+    
+    // Save list
+    static func saveMyList(_ name: String, _ color: UIColor) throws {
+        let myList = MyList(context: viewContext)
+        myList.name = name
+        myList.color = color
+    
+        try save()
+    }
+    
+    // Save data
+    static func save() throws {
+        try viewContext.save()
+    }
+    
+}
